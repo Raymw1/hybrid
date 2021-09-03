cf0160dee757da9def26470e8b210c6951437c91
Raymw1 <rayan.rmw@gmail.com>
2021-09-02 21:49:44 -0300

    Installing nunjucks module

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.send("Ok");
+  return res.render("login");
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const server = express();
 const routes = require("./routes/index");
+const nunjucks = require("nunjucks");
+const path = require("path");
 
+server.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, "app/views"), {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
 server.use(routes);
 
 const PORT = process.env.PORT || 3000;
