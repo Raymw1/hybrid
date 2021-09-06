6d30b21d699c1139fe2bc1d35d33d739968358c2
Raymw1 <rayan.rmw@gmail.com>
2021-09-06 19:48:48 -0300

    feature: Changing email error display

--- public/scripts/validateForm.js
+++ public/scripts/validateForm.js
@@ -2,43 +2,20 @@
 // =========== VALIDATE ===========
 const Validate = {
   errorBox: document.querySelector(".message-popup"),
-  apply(input, func) {
-    Validate.clearErrors();
-    const results = Validate[func](input.value);
-    input.value = results.value;
-    if (results.error) {
-      Validate.displayError(input, results.error);
-    }
-  },
-  clearErrors() {
-    if (Validate.errorBox.classList.contains("active")) {
-      Validate.errorBox.classList.remove("active");
-    }
-  },
-  displayError(input, error) {
-    Validate.errorBox.innerHTML = `<img src="/assets/images/error.png" alt="Erro" >${error}`;
-    Validate.errorBox.classList.add("active");
-    setTimeout(() => {
-      setTimeout(() => {
-        Validate.errorBox.innerText = "";
-      }, 400);
-      Validate.errorBox.classList.remove("active");
-    }, 4000);
-    input.focus();
-  },
-  isEmail(value) {
-    let error = null;
+  isEmail(input) {
+    let img = document.querySelector(".input img");
+    if (img)
+      document.querySelector(".input input").parentElement.removeChild(img);
+    // eslint-disable-next-line no-undef
+    img = new Image();
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!value.match(mailFormat)) {
-      error = "Email inválido!";
-    }
-    return {
-      error,
-      value,
-    };
+    img.src = !input.value.match(mailFormat)
+      ? "/assets/images/error.png"
+      : "/assets/images/success.svg";
+    input.parentElement.appendChild(img);
   },
   allFields(event) {
-    const fields = event.currentTarget.querySelectorAll(
+    const fields = event.target.parentElement.querySelectorAll(
       ".input-group input, .input-group select, .input-group textarea"
     );
     for (const field of fields) {
