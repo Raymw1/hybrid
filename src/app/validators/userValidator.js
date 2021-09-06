66e8426d14f70207039eab08ff698a675518da47
Raymw1 <rayan.rmw@gmail.com>
2021-09-05 22:19:04 -0300

    feature: Validate signup form in server side done

---
+++ src/app/validators/userValidator.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-useless-escape */
+const { verifyForm } = require("./mainValidator");
+const User = require("../models/User");
+
+module.exports = {
+  async post(req, res, next) {
+    const emptyFields = verifyForm(req.body);
+    // await checkIfIsAdminToCreate(req, res, () => {});
+    if (emptyFields)
+      return res.render("signup", {
+        ...emptyFields,
+      });
+    const { name, email, phone, password, city } = req.body;
+    const data = { name, email, phone, city };
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!email.match(mailFormat))
+      return res.render("signup", {
+        data,
+        error: "Insira um email válido",
+      });
+    const user = await User.findOne({ where: { email } });
+    if (user)
+      return res.render("signup", {
+        data,
+        error: "Email já cadastrado!",
+      });
+    // if (password !== passwordRepeat)
+    //   return res.render("signup", {
+    //     data,
+    //     error: "Senhas diferentes, tente novamente!",
+    //   });
+    next();
+  },
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,10 @@ const SessionController = require("../app/controllers/SessionController");
 const routes = express.Router();
 const UserController = require("../app/controllers/UserController");
 const sessionValidator = require("../app/validators/sessionValidator");
+const userValidator = require("../app/validators/userValidator");
 
 routes.get("/", UserController.index);
-routes.post("/signup", UserController.post);
+routes.post("/signup", userValidator.post, UserController.post);
 
 routes.post(
   "/forgot-password",
