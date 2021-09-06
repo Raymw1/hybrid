19e8b3ac23e5c71c28f48c84fdc5f219fcfa1ef4
Raymw1 <rayan.rmw@gmail.com>
2021-09-05 21:59:58 -0300

    feature: Validate forms function done

---
+++ public/scripts/mask.js
@@ -0,0 +1,40 @@
+const Mask = {
+  apply(input, func) {
+    setTimeout(() => {
+      input.value = Mask[func](input.value);
+    }, 1);
+  },
+  formatPhone(value) {
+    value = value.replace(/\D/g, ""); // Get only digits/numbers
+    value = value.replace(/(\d{1})(\d)/, "+$1$2"); // '+9999999999999'
+    value = value.replace(/(\d{2})(\d{2})(\d)/, "$1($2)$3"); // '+99(99)999999999'
+    value = value.replace(/(\d{5})(\d)/, "$1-$2"); // '+99(99)99999-9999'
+    return value;
+  },
+  formatBRL(value) {
+    value = value.replace(/\D/g, ""); // Get only digits/numbers
+    return new Intl.NumberFormat("pt-BR", {
+      // TO R$
+      style: "currency",
+      currency: "BRL",
+    }).format(value / 100);
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, "");
+
+    if (value.length > 14) {
+      value = value.slice(0, -1);
+    }
+    if (value.length > 11) {
+      value = value.replace(/(\d{2})(\d)/, "$1.$2"); // '11.222333444455'
+      value = value.replace(/(\d{3})(\d)/, "$1.$2"); // '11.222.333444455'
+      value = value.replace(/(\d{3})(\d)/, "$1/$2"); // '11.222.333/444455'
+      value = value.replace(/(\d{4})(\d)/, "$1-$2"); // '11.222.333/4444-55'
+    } else {
+      value = value.replace(/(\d{3})(\d)/, "$1.$2"); // '112.22333444'
+      value = value.replace(/(\d{3})(\d)/, "$1.$2"); // '112.223.33444'
+      value = value.replace(/(\d{3})(\d)/, "$1-$2"); // '112.223.334-44'
+    }
+    return value;
+  },
+};
---
+++ public/scripts/validateForm.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-useless-escape */
+// =========== VALIDATE ===========
+const Validate = {
+  errorBox: document.querySelector(".message-popup"),
+  apply(input, func) {
+    Validate.clearErrors();
+    const results = Validate[func](input.value);
+    input.value = results.value;
+    if (results.error) {
+      Validate.displayError(input, results.error);
+    }
+  },
+  clearErrors() {
+    if (Validate.errorBox.classList.contains("active")) {
+      Validate.errorBox.classList.remove("active");
+    }
+  },
+  displayError(input, error) {
+    Validate.errorBox.innerHTML = `<img src="/assets/images/error.png" alt="Erro" >${error}`;
+    Validate.errorBox.classList.add("active");
+    setTimeout(() => {
+      setTimeout(() => {
+        Validate.errorBox.innerText = "";
+      }, 400);
+      Validate.errorBox.classList.remove("active");
+    }, 4000);
+    input.focus();
+  },
+  isEmail(value) {
+    let error = null;
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!value.match(mailFormat)) {
+      error = "Email inválido!";
+    }
+    return {
+      error,
+      value,
+    };
+  },
+  allFields(event) {
+    const fields = document.querySelectorAll(
+      ".input-group input, .input-group select, .input-group textarea"
+    );
+    for (const field of fields) {
+      if (
+        field.value.trim() === "" &&
+        field.name !== "removed_files" &&
+        field.name !== "avatar" &&
+        field.name !== "photos"
+      ) {
+        event.preventDefault();
+        Validate.errorBox.innerHTML = `<img src="/assets/images/error.png" alt="Erro" >Por favor, preencha todos os campos!`;
+        Validate.errorBox.classList.add("active");
+        setTimeout(() => {
+          setTimeout(() => {
+            Validate.errorBox.innerText = "";
+          }, 400);
+          Validate.errorBox.classList.remove("active");
+        }, 4000);
+      }
+    }
+  },
+};
