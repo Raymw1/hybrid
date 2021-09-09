9ea32520538bb150c2a754e9835899db401118ab
Raymw1 <rayan.rmw@gmail.com>
2021-09-09 20:04:13 -0300

    feature: delete schedule modal done

---
+++ public/scripts/cancelSchedule.js
@@ -0,0 +1,17 @@
+const schedules = document.querySelectorAll("div.cancelSchedule");
+const modalOverlay = document.querySelector(".modal-overlay");
+const input = document.querySelector("input[type=hidden]");
+const modalDesk = document.querySelector(".modal-content span.modalDesk");
+const modalRoom = document.querySelector(".modal-content span.modalRoom");
+const modalUnity = document.querySelector(".modal-content span.modalUnity");
+const modalDate = document.querySelector(".modal-content span.modalDate");
+schedules.forEach((schedule) => {
+  schedule.addEventListener("click", () => {
+    input.value = schedule.dataset.id;
+    modalDesk.innerText = schedule.dataset.desk;
+    modalDate.innerText = schedule.dataset.date;
+    modalRoom.innerText = schedule.dataset.room;
+    modalUnity.innerText = schedule.dataset.unity;
+    modalOverlay.classList.add("active");
+  });
+});
--- src/app/services/scheduleServices.js
+++ src/app/services/scheduleServices.js
@@ -3,15 +3,14 @@
 const Room = require("../models/Room");
 const Desk = require("../models/Desk");
 const Schedule = require("../models/Schedule");
+const City = require("../models/City");
 const { parseDate } = require("../../lib/utils");
 
-async function getDeskPosition(desk_id) {
-  return (await Desk.find(desk_id)).position;
-}
-
-async function getRoom(desk_id) {
+async function getRoomAndUNity(desk_id) {
   const desk = await Desk.find(desk_id);
-  return (await Room.find(desk.room_id)).id;
+  const room = await Room.find(desk.room_id);
+  const unity = await City.find(room.city_id);
+  return { room: room.id, unity: unity.city };
 }
 
 async function getSchedules(user_id) {
@@ -20,8 +19,9 @@ async function getSchedules(user_id) {
     const schedulesPromise = schedules.map(async (schedule) => ({
       scheduleId: schedule.id,
       day: parseDate(schedule.schedule).birthday,
-      room: await getRoom(schedule.desk_id),
-      desk: await getDeskPosition(schedule.desk_id),
+      date: parseDate(schedule.schedule).dayAndMonth,
+      desk: (await Desk.find(schedule.desk_id)).position,
+      ...(await getRoomAndUNity(schedule.desk_id)),
     }));
     return Promise.all(schedulesPromise);
   } catch (err) {
