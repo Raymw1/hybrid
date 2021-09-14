ee0ab7c359161a5955c51e441f603faf4a35a3df
Raymw1 <rayan.rmw@gmail.com>
2021-09-13 21:52:31 -0300

    feature: redesign of schedule rooms

--- public/scripts/rooms.js
+++ public/scripts/rooms.js
@@ -1,21 +1,39 @@
-const desks = document.querySelectorAll(".desk");
-const input = document.querySelector("input[type=hidden]");
-const modalDesk = document.querySelector(".modal-content span.modalDesk");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal-content");
 const modalRoom = document.querySelector(".modal-content span.modalRoom");
-const buttonSchedule = document.querySelector("#desksForm .button");
-desks.forEach((desk) => {
-  desk.addEventListener("click", () => {
-    if (!desk.classList.contains("occuped")) {
-      desks.forEach((desk) => {
-        desk.classList.remove("active");
+const rooms = document.querySelectorAll(".room");
+let desks = null;
+rooms.forEach((room) => {
+  room.addEventListener("click", () => {
+    rooms.forEach((room) => {
+      room.classList.remove("active");
+    });
+    const modalDesks = modal.querySelector(".desks");
+    const modalButton = modal.querySelector("button");
+    if (modalDesks) modal.removeChild(modalDesks);
+    if (modalButton) modal.removeChild(modalButton);
+    desks = room.querySelector(".desks").cloneNode(true);
+    modal.append(desks);
+    // modal.append(button);
+    modalRoom.innerText = room.dataset.id;
+    modal.insertAdjacentHTML(
+      "beforeend",
+      `<button type="submit" form="desksForm" onclick="Validate.allFields(event)" class="button">Confirmar<img src="/assets/images/next.svg" alt=""></button>`
+    );
+    desks = document.querySelectorAll(".modal-content .desks .desk");
+    const input = document.querySelector("input[type=hidden]");
+    desks.forEach((desk) => {
+      desk.addEventListener("click", () => {
+        if (!desk.classList.contains("occuped")) {
+          desks.forEach((desk) => {
+            desk.classList.remove("active");
+          });
+          input.value = desk.dataset.id;
+          modalRoom.innerText = desk.dataset.roomid;
+          desk.classList.add("active");
+        }
       });
-      buttonSchedule.style.opacity = 1;
-      buttonSchedule.style.cursor = "pointer";
-      buttonSchedule.setAttribute("onclick", "Modal.open(event)");
-      input.value = desk.dataset.id;
-      modalDesk.innerText = desk.dataset.position;
-      modalRoom.innerText = desk.dataset.roomid;
-      desk.classList.add("active");
-    }
+    });
+    modalOverlay.classList.add("active");
   });
 });
